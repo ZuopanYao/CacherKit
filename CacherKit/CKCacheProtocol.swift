5f988e28b23dc9ae74c6eb5ddb2834895d9fc4d7
yaozuopan <yaozuopan@weuseapp.com>
2021-06-28 09:45:17 +0800

    fix bug

--- CacherKit/CKCacheProtocol.swift
+++ CacherKit/CKCacheProtocol.swift
@@ -14,13 +14,17 @@ protocol CKCacheProtocol {
     func array<T>(_ Type: T.Type) -> [T]?
     
     var url: URL? { get set }
-    var int: Int? { get set }
+    /// If the key doesn‘t exist, this method returns 0.
+    var int: Int { get set }
+    /// If the key doesn‘t exist, this method returns false.
     var bool: Bool { get set }
     var data: Data? { get set }
-    var float: Float? { get set }
+    /// If the key doesn‘t exist, this method returns 0.0.
+    var float: Float { get set }
     var array: [Any]? { get set }
     var string: String? { get set }
-    var double: Double? { get set }
+    /// If the key doesn‘t exist, this method returns 0.0.
+    var double: Double { get set }
     var stringArray: [String]? { get set }
     var dictionary: [String: Any]? { get set }
 }
